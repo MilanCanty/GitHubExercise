a78ffb7e392aac6f6982fc50832560a541cc7086
MilanCanty <milancanty@gmail.com>
2023-01-23 15:43:25 -0500

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Milan Canty on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
